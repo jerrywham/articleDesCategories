3f9fdf40298f5a7a6e6b2078eff22e039e6a8990
jerrywham <github@ecyseo.net>
2017-08-21 18:34:26 +0200

    Create fr.php

---
+++ lang/fr.php
@@ -0,0 +1,7 @@
+<?php 
+    $LANG = array(
+        "L_TITLE"            =>"articlesDesCategories",
+        "L_DESCRIPTION"      =>"Liens vers les articles d'une catégorie à afficher dans le menu",
+        "L_NB_ART"           =>"Nombre maximal d'articles à afficher",
+        "L_REC"              =>"Enregistrer",
+    );
